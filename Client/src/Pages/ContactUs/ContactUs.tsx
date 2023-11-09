7bfdcd4a15772f131522b10ade3b1253d811edc5
unknown <vamsiagnihotram98@gmail.com>
2023-11-09 02:48:43 +0200

    Frontend Pages Update 2

--- Client/src/Pages/ContactUs/ContactUs.tsx
+++ Client/src/Pages/ContactUs/ContactUs.tsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
     <p>Sri Ranga Pabbisetty : sriranga.pabbisetty@sjsu.edu</p>
     <p>Komal Kumar Vasireddy : komalkumar.vasireddy@sjsu.edu</p>
     <p>Surya Vamsi Krishna Pulapa : suryavamsikrishna.pulapa@sjsu.edu</p>
-    <p>Vamsi Krishna Agnihotram : vamsikrishna.agnihotram@sjsu.edu</p>
+    <p>Vamsi Krishna Agnihotram : vamsi.agnihotram@sjsu.edu</p>
   </div>;
 };
 
