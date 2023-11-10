3d8f75cb902e6a90f9b54c9f20b6c7de37bb4092
Checkout <Komal Kumar Vasireddy>
2023-11-09 23:04:41 -0800

    Created routes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import {AppBar, Autocomplete, Box, Tab, Tabs, TextField, Toolbar} from '@mui/material';
 import MovieIcon from '@mui/icons-material/Movie';
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const dummyArray = ["Jathi Ratnalu", "Bahubali", "Vikram"]
 const Header = () => {
     const [value, setValue] = useState(0);
-    return <AppBar sx={{bgcolor: "#2b2d42" }}>
+    return <AppBar position="sticky" sx={{bgcolor: "#2b2d42" }}>
         <Toolbar>
            <Box width={'20%'}>
                 <MovieIcon/>
@@ -25,9 +26,9 @@ const Header = () => {
 
             <Box display={"flex"}>
                 <Tabs textColor="inherit" indicatorColor="secondary" value={value} onChange={(e, val)=>setValue(val)}>
-                    <Tab label="Movies"/>
-                    <Tab label="Admin"/>
-                    <Tab label="Auth"/>
+                    <Tab LinkComponent={Link} to="/movies" label="Movies"/>
+                    <Tab LinkComponent={Link} to="/admin" label="Admin"/>
+                    <Tab LinkComponent={Link} to="/auth" label="Auth"/>
                 </Tabs>
             </Box>
 
